5820fdd55343edbacdbb373e96fbc5cd802aa3c4
Dmitry <zik-element@mail.ru>
2021-10-27 21:29:08 +0300

    dz1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Message } from './component/Message/Messege';
+
+const text = "prop"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Message message={text}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/component/Message/Messege.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Message = ({message}) => {
+    console.log(message)
+    return (
+        <h3>{message}</h3>
+    )
+}
